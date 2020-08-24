30400d6bfb4f62ea3ee085e905d1d745b1e53d6c
Christopher <Christopher.Fenner@me.com>
2020-08-24 21:19:02 +0200

    reduce api functions

--- src/kas-api.ts
+++ src/kas-api.ts
@@ -4,29 +4,7 @@ import { parseString } from "xml2js";
 import { APIResultTransformer } from "./api-result-transformer";
 
 type APIFunction =
-    "add_account" | "delete_account" | "get_accountressources" | "get_accounts" | "get_accountsettings"
-        | "get_server_information" | "update_account" | "update_accountsettings" | "update_superusersettings" |
-    "update_chown" |
-    "add_cronjob" | "delete_cronjob" | "get_cronjobs" | "update_cronjob" |
-    "add_database" | "delete_database" | "get_databases" | "update_database" |
-    "add_ddnsuser" | "delete_ddnsuser" | "get_ddnsusers" | "update_ddnsuser" |
-    "add_directoryprotection" | "delete_directoryprotection" | "get_directoryprotection"
-        | "update_directoryprotection" |
-    "add_dkim" | "delete_dkim" | "get_dkim" |
-    "add_dns_settings" | "delete_dns_settings" | "get_dns_settings" | "reset_dns_settings" | "update_dns_settings" |
-    "add_domain" | "delete_domain" | "get_domains" | "get_topleveldomains" | "move_domain" | "update_domain" |
-    "add_ftpusers" | "delete_ftpuser" | "get_ftpusers" | "update_ftpuser" |
-    "add_mailaccount" | "delete_mailaccount" | "get_mailaccounts" | "update_mailaccount" |
-    "add_mailstandardfilter" | "delete_mailstandardfilter" | "update_mailstandardfilter" |
-    "add_mailforward" | "delete_mailforward" | "get_mailforwards" | "update_mailforward" |
-    "add_mailinglist" | "delete_mailinglist" | "get_mailinglists" | "update_mailinglist" |
-    "add_sambauser" | "delete_sambauser" | "get_sambausers" | "update_sambauser" |
-    "add_session" |
-    "add_softwareinstall" | "get_softwareinstall" |
-    "update_ssl" |
-    "get_space" | "get_space_usage" | "get_traffic" |
-    "add_subdomain" | "delete_subdomain" | "get_subdomains" | "update_subdomain" |
-    "add_symlink";
+    "add_mailforward" | "delete_mailforward" | "get_mailforwards" | "update_mailforward";
 
 export class KasApi {
     private static endpoint = "https://kasapi.kasserver.com/soap/wsdl/KasApi.wsdl";
